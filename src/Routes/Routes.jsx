4932d77777b7f75191344e205bc4495d6359e060
fazlerabbi13 <mohammad0132794@gmail.com>
2024-03-09 11:24:54 +0600

    create a context for authentication

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,6 +6,7 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
+import Login from "../pages/Home/Login/Login";
 
 export const router = createBrowserRouter([
     {
@@ -24,6 +25,10 @@ export const router = createBrowserRouter([
                 path:'order/:category',
                 element:<Order></Order>
             },
+            {
+                path:'login',
+                element: <Login></Login>
+            }
         ]
     },
 ]);
--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,17 @@ import {
 import "./index.css";
 import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
+import Authprovider from './Provider/Authprovider';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className='max-w-screen-xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <Authprovider>
+      <HelmetProvider>
+        <div className='max-w-screen-xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </Authprovider>
   </React.StrictMode>,
 )
