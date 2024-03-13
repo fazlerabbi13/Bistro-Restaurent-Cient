0e47f1514b36043061bc74ae0ce3226ea5f21529
fazlerabbi13 <mohammad0132794@gmail.com>
2024-03-14 00:55:26 +0600

    google login added using firebase and saved the user

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,12 +1,25 @@
 import { useContext } from "react";
 import { FaGoogle } from "react-icons/fa";
 import { AuthContext } from "../../Provider/Authprovider";
+import useAxioxPublic from "../../hook/useAxioxPublic";
+import { useNavigate } from "react-router-dom";
 const SocialLogin = () => {
     const { googleLogin} = useContext(AuthContext);
+    const axiosPublic = useAxioxPublic();
+    const navigate = useNavigate();
     const handleSocialLogin = () =>{
         googleLogin()
         .then(result =>{
-            console.log(result.user)
+            console.log(result.user);
+            const userInfo = {
+                email:result.user?.email,
+                name:result.user?.displayName,
+            }
+            axiosPublic.post('/users', userInfo)
+            .then(res =>{
+                console.log(res.data)
+               navigate('/')
+            })
         })
     }
     return (
